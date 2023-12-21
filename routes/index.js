a6ea0ed03c596d17d60e8fe331f2c160303d83b8
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2023-12-21 15:49:48 -0500

    edited model constraints, pathing, and set up insomnia connection

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
-router.use((req, res)=>{
+router.use((req, res) => {
     return res.status(404).send('Not found');
 });
 
